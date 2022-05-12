7830b9897f3d5aede1e0e8fea668f01cc75ca932
Artracurs <0xdf00ff@gmail.com>
2022-05-12 10:43:24 +0500

    ooo

---
+++ typer/src/Components/InpitText/InpitText.jsx
@@ -0,0 +1,14 @@
+import s from "./InpitText.module.scss";
+import Texts from "../Texts/Texts"
+
+
+const InpitText = () => {
+  return (
+    <div className={s.container}>
+      <Texts />
+      <input type="text" name="" id="" />
+    </div>
+  );
+};
+
+export default InpitText;
---
+++ typer/src/Components/Texts/Texts.jsx
@@ -0,0 +1,11 @@
+import s from "./Texts.module.scss";
+
+const Texts = () => {
+  return (
+    <div className={s.container}>
+      <div className={s.stroke}> Hello text here </div>
+    </div>
+  );
+};
+
+export default Texts;
